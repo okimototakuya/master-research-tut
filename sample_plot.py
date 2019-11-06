c78182e1383e02631b1ea567ead835bac2b18cfe
沖本卓也 <okimototakuya@okimototakuyanoMacBook-Air.local>
2019-11-06 22:53:41 +0900

    横軸は整数のみ, プロット可, サンプルは標準入力で指定, 軸ラベル(Eng)追加

--- sample_plot.py
+++ sample_plot.py
@@ -5,12 +5,12 @@ x = []  # グラフの横軸(整数値)
 y = []  # グラフの縦軸
 EVAR = 0  # 整数値
 #EVAR = 1  # 時刻
-#TBAR = 2  # 鉛直方向の加速度(上が+)
-#TBAR = 3  # 前後方向の加速度(前が+)
-#TBAR = 4  # 左右方向の加速度(右が+)
-TBAR = 5  # 左右回転の角速度(右が+)
-#TBAR = 6  # 側方回転の角速度(右手側への回転が+)
-#TBAR = 7  # 前傾後傾の角速度(後傾が+)
+#TVAR = 2  # 鉛直方向の加速度(上が+)
+#TVAR = 3  # 前後方向の加速度(前が+)
+#TVAR = 4  # 左右方向の加速度(右が+)
+TVAR = 5  # 左右回転の角速度(右が+)
+#TVAR = 6  # 側方回転の角速度(右手側への回転が+)
+#TVAR = 7  # 前傾後傾の角速度(後傾が+)
 
 print("サンプルファイル名を入力してください↓")
 str = input()
@@ -28,7 +28,35 @@ with open("./" + str) as f:
         x.append(int(row[EVAR]))
       else:
         x.append(row[EVAR])
-      y.append(float(row[TBAR]))
+      y.append(float(row[TVAR]))
 
+if EVAR == 0:
+  #strx = "整数値"
+  strx = "Integer"
+else:
+  #strx = "時刻"
+  strx = "Time"
+
+if TVAR == 2:
+  #stry = "鉛直方向の加速度(上が+)"
+  stry = "Acceleration x[g]"
+elif TVAR == 3:
+  #stry = "前後方向の加速度(前が+)"
+  stry = "Acceleration y[g]"
+elif TVAR == 4:
+  #stry = "左右方向の加速度(右が+)"
+  stry = "Acceleration z[g]"
+elif TVAR == 5:
+  #stry = "左右回転の角速度(右が+)"
+  stry = "AngularRate x[dps]"
+elif TVAR == 6:
+  #stry = "左右回転の角速度(右が+)"
+  stry = "AngularRate y[dps]"
+else:
+  #stry ="前傾後傾の角速度(後傾が+)"
+  stry = "AngularRate z[dps]"
+ 
+plt.xlabel(strx)
+plt.ylabel(stry)
 plt.plot(x,y)
 plt.show()
