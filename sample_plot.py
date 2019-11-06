a6185d08959d4469159dd9ec6022d6187d8e4dff
沖本卓也 <okimototakuya@okimototakuyanoMacBook-Air.local>
2019-11-06 19:33:55 +0900

    グラフの横軸のリスト化

--- sample_plot.py
+++ sample_plot.py
@@ -1,4 +1,15 @@
 import csv
 import matplotlib.pyplot as plt
 
+x = []  # グラフの横軸(整数値)
 
+with open("./LOG_20181219141837_00010533_0021002B401733434E45.csv") as f:
+  #print(f.read())
+
+  reader = csv.reader(f)
+  #del reader[:3]
+
+  for row in reader:
+    #print(row[0])
+    x.append(row[0])
+  del x[:3]
