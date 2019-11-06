2c3586ff34f5ea5b17bfe8301e97e577d83a4b44
沖本卓也 <okimototakuya@okimototakuyanoMacBook-Air.local>
2019-11-06 21:55:41 +0900

    横軸は整数のみ, プロット可, サンプルはプログラム内で指定

--- sample_plot.py
+++ sample_plot.py
@@ -3,6 +3,15 @@ import matplotlib.pyplot as plt
 
 x = []  # グラフの横軸(整数値)
 y = []  # グラフの縦軸
+EVAR = 0  # 整数値
+#EVAR = 1  # 時刻
+#TBAR = 2  # 鉛直方向の加速度(上が+)
+#TBAR = 3  # 前後方向の加速度(前が+)
+#TBAR = 4  # 左右方向の加速度(右が+)
+TBAR = 5  # 左右回転の角速度(右が+)
+#TBAR = 6  # 側方回転の角速度(右手側への回転が+)
+#TBAR = 7  # 前傾後傾の角速度(後傾が+)
+
 
 with open("./LOG_20181219141837_00010533_0021002B401733434E45.csv") as f:
 
@@ -14,8 +23,11 @@ with open("./LOG_20181219141837_00010533_0021002B401733434E45.csv") as f:
     if i <= 3:
       pass
     else:
-      x.append(int(row[0]))
-      y.append(float(row[2]))
+      if EVAR == 0:
+        x.append(int(row[EVAR]))
+      else:
+        x.append(row[EVAR])
+      y.append(float(row[TBAR]))
 
 plt.plot(x,y)
 plt.show()
