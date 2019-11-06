5a1e0f3513265ccfe2251eef3c37f4125e7961ab
沖本卓也 <okimototakuya@okimototakuyanoMacBook-Air.local>
2019-11-06 19:42:00 +0900

    グラフの縦軸のリスト化

--- sample_plot.py
+++ sample_plot.py
@@ -2,6 +2,7 @@ import csv
 import matplotlib.pyplot as plt
 
 x = []  # グラフの横軸(整数値)
+y = []  # グラフの縦軸
 
 with open("./LOG_20181219141837_00010533_0021002B401733434E45.csv") as f:
   #print(f.read())
@@ -12,4 +13,7 @@ with open("./LOG_20181219141837_00010533_0021002B401733434E45.csv") as f:
   for row in reader:
     #print(row[0])
     x.append(row[0])
+    y.append(row[2])
   del x[:3]
+  del y[:3]
+  print(y)
