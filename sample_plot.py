3f998907d4c1458333190b3effe248d1c0d662da
沖本卓也 <okimototakuya@okimototakuyanoMacBook-Air.local>
2019-11-07 17:34:53 +0900

    凡例の表示

--- sample_plot.py
+++ sample_plot.py
@@ -24,11 +24,11 @@ with open("./" + str, mode='r') as f:
     else:         # 横軸が時刻表示
       x.append(row[EVAR])
 
-    if TVAR == 0: # 縦軸が加速度
+    if TVAR == 0: # 縦軸が加速度[g]
       y1.append(float(row[2]))
       y2.append(float(row[3]))
       y3.append(float(row[4]))
-    else:         # 縦軸が角速度
+    else:         # 縦軸が角速度[dps]
       y1.append(float(row[5]))
       y2.append(float(row[6]))
       y3.append(float(row[7]))
@@ -50,7 +50,8 @@ else:
 plt.title(str)
 plt.xlabel(strx)
 plt.ylabel(stry)
-plt.plot(x,y1)
-plt.plot(x,y2)
-plt.plot(x,y3)
+plt.plot(x,y1,label="x")
+plt.plot(x,y2,label="y")
+plt.plot(x,y3,label="z")
+plt.legend()  # 凡例の表示
 plt.show()
