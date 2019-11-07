c029b7338cc8b4b4ba6b88b518103c422d76e2eb
沖本卓也 <okimototakuya@okimototakuyanoMacBook-Air.local>
2019-11-07 16:36:59 +0900

    最初のコミット

--- sample_plot.py
+++ sample_plot.py
@@ -7,12 +7,8 @@ y = []  # グラフの縦軸
 EVAR = 0  # 整数値
 #EVAR = 1  # 時刻
 
-#TVAR = 2  # 鉛直方向の加速度(上が+)
-#TVAR = 3  # 前後方向の加速度(前が+)
-#TVAR = 4  # 左右方向の加速度(右が+)
-#TVAR = 5  # 左右回転の角速度(右が+)
-#TVAR = 6  # 側方回転の角速度(右手側への回転が+)
-TVAR = 7  # 前傾後傾の角速度(後傾が+)
+TVAR = 0  # 加速度
+#TVAR = 1  # 角速度
 
 print("サンプルファイル名を入力してください↓")
 str = input()
@@ -27,9 +23,6 @@ with open("./" + str, mode='r') as f:
       x.append(row[EVAR])
     y.append(float(row[TVAR]))
     
-print(x[:10])
-print(y[:10])
-
 if EVAR == 0:
   #strx = "整数値"
   strx = "Integer"
@@ -37,26 +30,14 @@ else:
   #strx = "時刻"
   strx = "Time"
 
-if TVAR == 2:
-  #stry = "鉛直方向の加速度(上が+)"
-  stry = "Acceleration x[g]"
-elif TVAR == 3:
-  #stry = "前後方向の加速度(前が+)"
-  stry = "Acceleration y[g]"
-elif TVAR == 4:
-  #stry = "左右方向の加速度(右が+)"
-  stry = "Acceleration z[g]"
-elif TVAR == 5:
-  #stry = "左右回転の角速度(右が+)"
-  stry = "AngularRate x[dps]"
-elif TVAR == 6:
-  #stry = "左右回転の角速度(右が+)"
-  stry = "AngularRate y[dps]"
+if TVAR == 0:
+  #stry = "加速度x/y/z[g]"
+  stry = "Acceleration x/y/z[g]"
 else:
-  #stry ="前傾後傾の角速度(後傾が+)"
-  stry = "AngularRate z[dps]"
+  #stry = "角速度x/y/z[dps]"
+  stry = "AngularRate x/y/z[dps]"
  
-plt.title(str)
+plt.title("サンプル名:"+str)
 plt.xlabel(strx)
 plt.ylabel(stry)
 plt.plot(x,y)
