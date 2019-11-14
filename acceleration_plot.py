ab94de827a719b5dbe64b96a15fedb7496c6c5bd
沖本卓也 <okimototakuya@okimototakuyanoMacBook-Air.local>
2019-11-14 18:19:52 +0900

    時系列グラフ(年月日は余計)

--- acceleration_plot.py
+++ acceleration_plot.py
@@ -2,7 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def main():
-  print("サンプルファイル名を入力してください↓")
+  print("サンプルファイル名を入力してください")
+  print(":", end="")
   filename = input()
 
   col_names = ['line', 'time', 
@@ -13,17 +14,19 @@ def main():
   df = pd.read_csv(filename,
                    names=col_names,
                    skiprows=3, 
-                   #index_col=0,
+                   index_col=1, # 0:整数値, 1:時刻
                    converters={'line':int, 'time':str, 
                                'Acceleration_x':float, 'Acceleration_y':float, 'Acceleration_z':float,
                                'AngularRate_x':float, 'AngularRate_y':float, 'AngularRate_z':float,
-                               'Temperture':float, 'Pressure':float, 'MagnetCount':int, 'MagnetSwitch':int,}
+                               'Temperture':float, 'Pressure':float, 'MagnetCount':int, 'MagnetSwitch':int,
+                              }
                   )
-  #print(df)
+  df.index = pd.to_datetime(df.index, format='%H:%M:%S.%f')
   #ax1 = df.plot(x='time', y='Acceleration_x') 
   #ax2 = df.plot(y='Acceleration_y', ax=ax1) 
   #ax3 = df.plot(y='Acceleration_z', ax=ax2) 
-  ax1 = df.plot(x='time', y='AngularRate_x') 
+  #ax1 = df.plot(x='line', y='AngularRate_x') 
+  ax1 = df.plot(y='AngularRate_x') 
   ax2 = df.plot(y='AngularRate_y', ax=ax1) 
   ax3 = df.plot(y='AngularRate_z', ax=ax2) 
   ax3.set_title(filename)
