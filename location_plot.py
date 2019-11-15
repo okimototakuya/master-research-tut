554eaa41b7bb5a00764159401db26ea67a83b2b1
沖本卓也 <okimototakuya@okimototakuyanoMacBook-Air.local>
2019-11-16 01:58:19 +0900

    新コメントアウト追加/余計なコメントアウト削除

--- location_plot.py
+++ location_plot.py
@@ -32,13 +32,14 @@ filename = "location_20181219_19.csv"
 #start = "2018-12-19 07:41:13+09"
 #end = "2018-12-19 08:00:29+09"
 # 校内
-start = "2018-12-19 09:38:05+09"
-end = "2018-12-19 13:58:05+09"
+#start = "2018-12-19 09:38:05+09"
+#end = "2018-12-19 13:58:05+09"
 # 下校
-#start = "2018-12-19 15:57:47+09"
-#end = "2018-12-19 16:07:49+09"
+start = "2018-12-19 15:57:47+09"
+end = "2018-12-19 16:07:49+09"
 
 def main():
+  # 列名を明示的に指定することにより, 欠損値をNaNで補完.
   col_names = ['btx_id', 'timestamp', 
                'major', 'minor', 'pos_id',
                'deviceid', 'lat', 'lon',
@@ -46,10 +47,11 @@ def main():
               ]
   df = pd.read_csv(filename,
                    names=col_names,
-                   #parse_dates=['recvDate'],
+                   ## 原データの時刻表示の余計な部分を削るため, 一旦str型で読込.
+                   #parse_dates=['recvDate'], 
                    skiprows=1, 
-                   index_col=8,
-                   #index_col=6,
+                   index_col=8, # recvData(時刻)
+                   #index_col=6,  # lat(緯度)
                    converters={'btx_id':int, 'timestamp':float, 
                                'major':int, 'minor':int, 'pos_id':int,
                                'deviceid':int, 'lat':float, 'lon':float,
@@ -57,23 +59,16 @@ def main():
                               }
                   )
 
+  # indexをpd.to_datetime()でDateTimeIndexにした.
+  # df.loc[] : error → Jupyter実践入門(pandas0.19.2)と異なる
+  # df.iloc[] : error
   df.index = [i[:19] for i in df.index] # recvDate:「+09」の削除
   df.index = pd.to_datetime(df.index) # DataFrame型のインデックス:string型 → DataTime型
-  #print(sin(radians(df.loc[:, 'lat']-35)))
-  #print(sin(radians(df.loc[:, 'lon']-135)))
+  # Series型の各要素への関数適用:基本演算(+,-など) +,- / 一般的な関数(三角関数など) map
   df.loc[:, 'lat'] = (df.loc[:, 'lat']-35).map(radians).map(sin)
   df.loc[:, 'lon'] = (df.loc[:, 'lon']-137).map(radians).map(sin)
-  #print(df)
-  #print(sin(radians(df.loc[:, 'lon']-135)))
 
-  #print(df.index[:6])
-  #print(df.loc[07:37:22:08:00:00 ,'lat'])
-  #print(df.loc['2018-12-19 07:37:22':'2018-12-19 07:47:10', 'lat'])
-  #print(df.index['2018-12-19 07:37:22':'2018-12-19 07:47:10'])
-  #print(df.index[0:10])
-  #print(df.loc[:'2018-12-19 07:47:10'])
   df_time = df.loc[start:end]
-  #print(df_time)
 
   #ax1 = (df_time).plot(y=['lat']) 
   #ax2 = (df_time).plot(y='lon', secondary_y=['lat','lon'], ax=ax1) 
@@ -81,8 +76,9 @@ def main():
   #ax.set_ylabel('lat')
   #ax.right_ax.set_ylabel('lon')
 
-  ax = (df_time).plot(x='lat', y='lon')
-  #ax = (df_time).plot(y=['lon'])
+  # DataFrame型plot() : xのラベル名(列名)は[]で囲まない. 
+  # yはどっちでも.→ ラベル/リスト
+  ax = (df_time).plot(x='lat', y=['lon'])
 
   plt.show()
 
