fe32dcaeaa0cf870c6e7c6daba78f5f876f6abbf
沖本卓也 <okimototakuya@okimototakuyanoMacBook-Air.local>
2019-11-15 20:09:35 +0900

    時間帯を指定して, グラフ表示(終わりのみ指定)

--- location_plot.py
+++ location_plot.py
@@ -5,6 +5,11 @@ def main():
   print("サンプルファイル名を入力してください")
   print(":", end="")
   filename = input()
+  print("表示する時間帯を入力してください")
+  print(":", end="")
+  time = input()
+  #print(filename)
+  #print(time)
 
   col_names = ['btx_id', 'timestamp', 
                'major', 'minor', 'pos_id',
@@ -25,8 +30,17 @@ def main():
 
   df.index = [i[:19] for i in df.index] # recvDate:「+09」の削除
   df.index = pd.to_datetime(df.index) # DataFrame型のインデックス:string型 → DataTime型
-  ax1 = df.plot(y='lat') 
-  ax2 = df.plot(y='lon', secondary_y=['lat','lon'], ax=ax1) 
+  #print(df.index[:6])
+  #print(df.loc[07:37:22:08:00:00 ,'lat'])
+  #print(df.loc['2018-12-19 07:37:22':'2018-12-19 07:47:10', 'lat'])
+  #print(df.index['2018-12-19 07:37:22':'2018-12-19 07:47:10'])
+  #print(df.index[0:10])
+  #print(df.loc[:'2018-12-19 07:47:10'])
+  df_time = df.loc[:time]
+  #print(df_time)
+
+  ax1 = (df_time).plot(y=['lat']) 
+  ax2 = (df_time).plot(y='lon', secondary_y=['lat','lon'], ax=ax1) 
   ax2.set_title(filename)
   #ax.set_ylabel('lat')
   #ax.right_ax.set_ylabel('lon')
