c514707fc3ca995ba1ca11c5b4023b744bfff338
沖本卓也 <okimototakuya@okimototakuyanoMacBook-Air.local>
2019-11-16 19:17:42 +0900

    3dplot.pyから他pythonファイルの実行:モジュールプログラミング

---
+++ 3dplot.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import acceleration_plot as ap 
+import location_plot as lp
+
+def main():
+  lp.main()
+
+if __name__ == '__main__':
+  main()
