bdbc910b8f2b1aad63866a819775f635341d4543
沖本卓也 <okimototakuya@okimototakuyanoMacBook-Air.local>
2019-11-16 21:24:47 +0900

    location_plot.py:オブジェクト指向(DataFrame, plot) → エラー:init()は引数を取らないのに, １つ引数を与えている(self, オブジェクト指向, lambda)

--- location_plot.py
+++ location_plot.py
@@ -38,56 +38,72 @@ end = "2018-12-19 16:07:49+09"
 #end = "2018-12-19 16:07:49+09"
 """
 
-def plotTimeLatLon(df):
-  ## 緯度(lat)経度(lon)の時系列変化をプロット
-  df_time = df.loc[start:end]
-  ax1 = (df_time).plot(y=['lat']) 
-  ax2 = (df_time).plot(y='lon', secondary_y=['lat','lon'], ax=ax1) 
-  ax2.set_title(filename)
-  #ax.set_ylabel('lat')
-  #ax.right_ax.set_ylabel('lon')
-  plt.show()
+class dataframe_maker():
+  df = None # DataFrame型インスタンスを格納
 
-def plotLatLon(df):
-  ## 緯度(lat)経度(lon)の射影から,２次元位置座標をプロット
-  # Series型の各要素への関数適用:基本演算(+,-など) +,- / 一般的な関数(三角関数など) map
-  df.loc[:, 'lat'] = (df.loc[:, 'lat']-35).map(radians).map(sin)
-  df.loc[:, 'lon'] = (df.loc[:, 'lon']-137).map(radians).map(sin)
-  df_time = df.loc[start:end]
-  # DataFrame型plot() : xのラベル名(列名)は[]で囲まない. 
-  # yはどっちでも.→ ラベル/リスト
-  ax = (df_time).plot(x='lat', y=['lon'])
-  plt.show()
+  def init():
+    # 列名を明示的に指定することにより, 欠損値をNaNで補完.
+    col_names = ['btx_id', 'timestamp', 
+                 'major', 'minor', 'pos_id',
+                 'deviceid', 'lat', 'lon',
+                 'recvDate',
+                ]
+    self.df = pd.read_csv(filename,
+                     names=col_names,
+                     ## 原データの時刻表示の余計な部分を削るため, 一旦str型で読込.
+                     #parse_dates=['recvDate'], 
+                     skiprows=1, 
+                     index_col=8, # recvData(時刻)
+                     #index_col=6,  # lat(緯度)
+                     converters={'btx_id':int, 'timestamp':float, 
+                                 'major':int, 'minor':int, 'pos_id':int,
+                                 'deviceid':int, 'lat':float, 'lon':float,
+                                 'recvDate':str,
+                                }
+                    )
+
+    # indexをpd.to_datetime()でDateTimeIndexにした.
+    # df.loc[] : error → Jupyter実践入門(pandas0.19.2)と異なる
+    # df.iloc[] : error
+    (self.df).index = [i[:19] for i in (self.df).index] # recvDate:「+09」の削除
+    (self.df).index = pd.to_datetime((self.df).index) # DataFrame型のインデックス:string型 → DataTime型
+
+  def makeTimeLatLon():
+    self.df = (self.df).loc[start:end]
+        
+  def makeLatLon():
+    # Series型の各要素への関数適用:基本演算(+,-など) +,- / 一般的な関数(三角関数など) map
+    (self.df).loc[:, 'lat'] = ((self.df).loc[:, 'lat']-35).map(radians).map(sin)
+    (self.df).loc[:, 'lon'] = ((self.df).loc[:, 'lon']-137).map(radians).map(sin)
+    self.df = (self.df).loc[start:end]
+
+
+class dataframe_plotter():
+  def plotTimeLatLon(df):
+    ## 緯度(lat)経度(lon)の時系列変化をプロット
+    ax1 = df.plot(y=['lat']) 
+    ax2 = df.plot(y='lon', secondary_y=['lat','lon'], ax=ax1) 
+    ax2.set_title(filename)
+    #ax.set_ylabel('lat')
+    #ax.right_ax.set_ylabel('lon')
+    plt.show()
+
+  def plotLatLon(df):
+    ## 緯度(lat)経度(lon)の射影から,２次元位置座標をプロット
+    # DataFrame型plot() : xのラベル名(列名)は[]で囲まない. 
+    # yはどっちでも.→ ラベル/リスト
+    ax = df.plot(x='lat', y=['lon'])
+    plt.show()
+
 
 def main():
-  # 列名を明示的に指定することにより, 欠損値をNaNで補完.
-  col_names = ['btx_id', 'timestamp', 
-               'major', 'minor', 'pos_id',
-               'deviceid', 'lat', 'lon',
-               'recvDate',
-              ]
-  df = pd.read_csv(filename,
-                   names=col_names,
-                   ## 原データの時刻表示の余計な部分を削るため, 一旦str型で読込.
-                   #parse_dates=['recvDate'], 
-                   skiprows=1, 
-                   index_col=8, # recvData(時刻)
-                   #index_col=6,  # lat(緯度)
-                   converters={'btx_id':int, 'timestamp':float, 
-                               'major':int, 'minor':int, 'pos_id':int,
-                               'deviceid':int, 'lat':float, 'lon':float,
-                               'recvDate':str,
-                              }
-                  )
-
-  # indexをpd.to_datetime()でDateTimeIndexにした.
-  # df.loc[] : error → Jupyter実践入門(pandas0.19.2)と異なる
-  # df.iloc[] : error
-  df.index = [i[:19] for i in df.index] # recvDate:「+09」の削除
-  df.index = pd.to_datetime(df.index) # DataFrame型のインデックス:string型 → DataTime型
-
-  plotTimeLatLon(df) # 緯度(lat)経度(lon)の時系列変化をプロット
+  #plotTimeLatLon(df) # 緯度(lat)経度(lon)の時系列変化をプロット
   #plotLatLon(df)  # 緯度(lat)経度(lon)の射影から,２次元位置座標をプロット
+  dm = dataframe_maker()
+  dm.init()
+  dm.makeLatLon()
+  dp = dataframe_plotter()
+  dp.plotLatLon(dm.df)
 
 if __name__ == '__main__':
   main()
