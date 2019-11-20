15cf971236f07ac7d954681a7c756f97417f945d
沖本卓也 <okimototakuya@okimototakuyanoMacBook-Air.local>
2019-11-20 21:12:02 +0900

    acceleration_plot.py:オブジェクト指向で書き換え

--- acceleration_plot.py
+++ acceleration_plot.py
@@ -1,41 +1,72 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def main():
-  print("サンプルファイル名を入力してください")
-  print(":", end="")
-  filename = input()
+## ID16
+# ファイル名
+filename = "LOG_20181219141837_00010533_0021002B401733434E45.csv"
 
-  col_names = ['line', 'time', 
-               'Acceleration_x', 'Acceleration_y', 'Acceleration_z',
-               'AngularRate_x', 'AngularRate_y', 'AngularRate_z',
-               'Temperture', 'Pressure', 'MagnetCount', 'MagnetSwitch',
-              ]
-  df = pd.read_csv(filename,
-                   names=col_names,
-                   skiprows=3, 
-                   parse_dates=['time'],
-                   index_col=1, # 0:整数値, 1:時刻
-                   converters={'line':int, 'time':str, 
-                               'Acceleration_x':float, 'Acceleration_y':float, 'Acceleration_z':float,
-                               'AngularRate_x':float, 'AngularRate_y':float, 'AngularRate_z':float,
-                               'Temperture':float, 'Pressure':float, 'MagnetCount':int, 'MagnetSwitch':int,
-                              }
-                  )
-  #ax1 = df.plot(y='Acceleration_x') 
-  #ax2 = df.plot(y='Acceleration_y', ax=ax1) 
-  #ax3 = df.plot(y='Acceleration_z', ax=ax2) 
-  #ax1 = df.plot(y='AngularRate_x') 
-  #ax2 = df.plot(y='AngularRate_y', ax=ax1) 
-  #ax3 = df.plot(y='AngularRate_z', ax=ax2) 
-  ax1 = df.plot(y='Acceleration_x') 
-  ax2 = df.plot(y='Acceleration_y', ax=ax1) 
-  ax3 = df.plot(y='Acceleration_z', ax=ax2) 
-  ax4 = df.plot(y='AngularRate_x', ax=ax3) 
-  ax5 = df.plot(y='AngularRate_y', ax=ax4) 
-  ax6 = df.plot(y='AngularRate_z', secondary_y=['Acceleration_x','AngularRate_x'], ax=ax5) 
-  ax6.set_title(filename)
-  plt.show()
+## ID19
+# ファイル名
+#filename = "LOG_20181219141901_00007140_00140064401733434E45.csv"
+
+class dataframe_maker():
+  df = None # DataFrame型インスタンスを格納
+
+  def init(self):
+    # 列名を明示的に指定することにより, 欠損値をNaNで補完.
+    col_names = ['line', 'time', 
+                 'Acceleration_x', 'Acceleration_y', 'Acceleration_z',
+                 'AngularRate_x', 'AngularRate_y', 'AngularRate_z',
+                 'Temperture', 'Pressure', 'MagnetCount', 'MagnetSwitch',
+                ]
+    self.df = pd.read_csv(filename,
+                     names=col_names,
+                     skiprows=3, 
+                     parse_dates=['time'],
+                     index_col=1, # 0:整数値, 1:時刻
+                     converters={'line':int, 'time':str, 
+                                 'Acceleration_x':float, 'Acceleration_y':float, 'Acceleration_z':float,
+                                 'AngularRate_x':float, 'AngularRate_y':float, 'AngularRate_z':float,
+                                 'Temperture':float, 'Pressure':float, 'MagnetCount':int, 'MagnetSwitch':int,
+                                }
+                    )
+
+class dataframe_plotter():
+  def plotTimeAcc(self, df):
+    ## 加速度の時系列変化をプロット
+    ax1 = df.plot(y='Acceleration_x') 
+    ax2 = df.plot(y='Acceleration_y', ax=ax1) 
+    ax3 = df.plot(y='Acceleration_z', ax=ax2) 
+    ax3.set_title(filename)
+    plt.show()
+
+  def plotTimeAng(self, df):
+    ## 角速度の時系列変化をプロット
+    ax1 = df.plot(y='AngularRate_x') 
+    ax2 = df.plot(y='AngularRate_y', ax=ax1) 
+    ax3 = df.plot(y='AngularRate_z', ax=ax2) 
+    ax3.set_title(filename)
+    plt.show()
+
+  def plotTimeAccAng(self, df):
+    ## 加速度・角速度の時系列変化をプロット
+    ax1 = df.plot(y='Acceleration_x') 
+    ax2 = df.plot(y='Acceleration_y', ax=ax1) 
+    ax3 = df.plot(y='Acceleration_z', ax=ax2) 
+    ax4 = df.plot(y='AngularRate_x', ax=ax3) 
+    ax5 = df.plot(y='AngularRate_y', ax=ax4) 
+    ax6 = df.plot(y='AngularRate_z', secondary_y=['Acceleration_x','AngularRate_x'], ax=ax5) 
+    ax6.set_title(filename)
+    plt.show()
+    
+    
+def main():
+  dm = dataframe_maker()
+  dm.init()
+  dp = dataframe_plotter()
+  dp.plotTimeAcc(dm.df)
+  #dp.plotTimeAng(dm.df)
+  #dp.plotTimeAccAng(dm.df)
 
 if __name__ == '__main__':
   main()
