3827f29f888ea73c0573548ca773dc605db5599a
沖本卓也 <okimototakuya@okimototakuyanoMacBook-Air.local>
2019-12-11 20:15:08 +0900

    location_plot.pyのインデント整形

--- location_plot.py
+++ location_plot.py
@@ -39,84 +39,84 @@ end = "2018-12-19 16:07:49+09"
 """
 
 class dataframe_maker():
-  df = None # DataFrame型インスタンスを格納
+	df = None # DataFrame型インスタンスを格納
 
-  def init(self):
-    # 列名を明示的に指定することにより, 欠損値をNaNで補完.
-    col_names = ['btx_id', 'timestamp', 
-                 'major', 'minor', 'pos_id',
-                 'deviceid', 'lat', 'lon',
-                 'recvDate',
-                ]
-    self.df = pd.read_csv(filename,
-                     names=col_names,
-                     ## 原データの時刻表示の余計な部分を削るため, 一旦str型で読込.
-                     #parse_dates=['recvDate'], 
-                     skiprows=1, 
-                     index_col=8, # recvData(時刻)
-                     #index_col=6,  # lat(緯度)
-                     converters={'btx_id':int, 'timestamp':float, 
-                                 'major':int, 'minor':int, 'pos_id':int,
-                                 'deviceid':int, 'lat':float, 'lon':float,
-                                 'recvDate':str,
-                                }
-                    )
+	def init(self):
+		# 列名を明示的に指定することにより, 欠損値をNaNで補完.
+		col_names = ['btx_id', 'timestamp',
+						'major', 'minor', 'pos_id',
+						'deviceid', 'lat', 'lon',
+						'recvDate',
+						]
+		self.df = pd.read_csv(filename,
+									names=col_names,
+									## 原データの時刻表示の余計な部分を削るため, 一旦str型で読込.
+									#parse_dates=['recvDate'], 
+									skiprows=1, 
+									index_col=8, # recvData(時刻)
+									#index_col=6,  # lat(緯度)
+									converters={'btx_id':int, 'timestamp':float, 
+													'major':int, 'minor':int, 'pos_id':int,
+													'deviceid':int, 'lat':float, 'lon':float,
+													'recvDate':str,
+													}
+									)
 
-    # indexをpd.to_datetime()でDateTimeIndexにした.
-    # df.loc[] : error → Jupyter実践入門(pandas0.19.2)と異なる
-    # df.iloc[] : error
-    (self.df).index = [i[:19] for i in (self.df).index] # recvDate:「+09」の削除
-    (self.df).index = pd.to_datetime((self.df).index) # DataFrame型のインデックス:string型 → DataTime型
+		# indexをpd.to_datetime()でDateTimeIndexにした.
+		# df.loc[] : error → Jupyter実践入門(pandas0.19.2)と異なる
+		# df.iloc[] : error
+		(self.df).index = [i[:19] for i in (self.df).index] # recvDate:「+09」の削除
+		(self.df).index = pd.to_datetime((self.df).index) # DataFrame型のインデックス:string型 → DataTime型
 
-  def makeTimeLatLon(self):
-    self.df = (self.df).loc[start:end]
-        
-  def makeLatLon(self):
-    # Series型の各要素への関数適用:基本演算(+,-など) +,- / 一般的な関数(三角関数など) map
-    (self.df).loc[:, 'lat'] = ((self.df).loc[:, 'lat']-35).map(radians).map(sin)
-    (self.df).loc[:, 'lon'] = ((self.df).loc[:, 'lon']-137).map(radians).map(sin)
-    self.df = (self.df).loc[start:end]
+	def makeTimeLatLon(self):
+		self.df = (self.df).loc[start:end]
+
+	def makeLatLon(self):
+		# Series型の各要素への関数適用:基本演算(+,-など) +,- / 一般的な関数(三角関数など) map
+		(self.df).loc[:, 'lat'] = ((self.df).loc[:, 'lat']-35).map(radians).map(sin)
+		(self.df).loc[:, 'lon'] = ((self.df).loc[:, 'lon']-137).map(radians).map(sin)
+		self.df = (self.df).loc[start:end]
 
 
 class dataframe_plotter():
-  def plotTimeLatLon(self, df):
-    ## 緯度(lat)経度(lon)の時系列変化をプロット
-    #ax1 = df.plot(y=['lat']) 
-    #ax2 = df.plot(y='lon', secondary_y=['lat','lon'], ax=ax1) 
-    ax1 = df.plot(y=['lat']) 
-    ax2 = df.plot(y='lon', secondary_y=['lat','lon'], ax=ax1) 
-    ax2.set_title(filename)
-    #ax.set_ylabel('lat')
-    #ax.right_ax.set_ylabel('lon')
-    plt.show()
+	def plotTimeLatLon(self, df):
+		## 緯度(lat)経度(lon)の時系列変化をプロット
+		#ax1 = df.plot(y=['lat']) 
+		#ax2 = df.plot(y='lon', secondary_y=['lat','lon'], ax=ax1) 
+		ax1 = df.plot(y=['lat']) 
+		ax2 = df.plot(y='lon', secondary_y=['lat','lon'], ax=ax1) 
+		ax2.set_title(filename)
+		#ax.set_ylabel('lat')
+		#ax.right_ax.set_ylabel('lon')
+		plt.show()
 
-  def plotLatLon(self, df):
-    ## 緯度(lat)経度(lon)の射影から,２次元位置座標をプロット
-    # DataFrame型plot() : xのラベル名(列名)は[]で囲まない. 
-    # yはどっちでも.→ ラベル/リスト
-    
-    ## 線分補間グラフ
-    ax = df.plot(x='lat', y=['lon'])
-    plt.show()
+	def plotLatLon(self, df):
+		## 緯度(lat)経度(lon)の射影から,２次元位置座標をプロット
+		# DataFrame型plot() : xのラベル名(列名)は[]で囲まない. 
+		# yはどっちでも.→ ラベル/リスト
 
-    """
-    ## 散布図グラフ
-    fig = plt.figure(figsize=(5,5))
-    ax = fig.add_subplot(111)
-    ax.scatter(df.loc[:,'lat'], df.loc[:,'lon'])
-    plt.xlim(-0.0001, 0.0001)
-    plt.ylim(-0.0001, 0.0001)
-    plt.show()
-    """
+		## 線分補間グラフ
+		ax = df.plot(x='lat', y=['lon'])
+		plt.show()
+
+		"""
+		## 散布図グラフ
+		fig = plt.figure(figsize=(5,5))
+		ax = fig.add_subplot(111)
+		ax.scatter(df.loc[:,'lat'], df.loc[:,'lon'])
+		plt.xlim(-0.0001, 0.0001)
+		plt.ylim(-0.0001, 0.0001)
+		plt.show()
+		"""
 
 def main():
-  #plotTimeLatLon(df) # 緯度(lat)経度(lon)の時系列変化をプロット
-  #plotLatLon(df)  # 緯度(lat)経度(lon)の射影から,２次元位置座標をプロット
-  dm = dataframe_maker()
-  dm.init()
-  dm.makeLatLon()
-  dp = dataframe_plotter()
-  dp.plotLatLon(dm.df)
+	#plotTimeLatLon(df) # 緯度(lat)経度(lon)の時系列変化をプロット
+	#plotLatLon(df)  # 緯度(lat)経度(lon)の射影から,２次元位置座標をプロット
+	dm = dataframe_maker()
+	dm.init()
+	dm.makeLatLon()
+	dp = dataframe_plotter()
+	dp.plotLatLon(dm.df)
 
 if __name__ == '__main__':
-  main()
+	main()
