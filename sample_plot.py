8cffa0243bfa51f8236074b03e897b3e06c4fd48
沖本卓也 <okimototakuya@okimototakuyanoMacBook-Air.local>
2019-12-11 20:02:16 +0900

    sample_plot.pyのインデント整形

--- sample_plot.py
+++ sample_plot.py
@@ -15,38 +15,36 @@ TVAR = 0  # 加速度[g]
 print("サンプルファイル名を入力してください↓")
 str = input()
 with open("./" + str, mode='r') as f:
+	reader = list(csv.reader(f))  # csv.readerはインデックス作成をサポートしていない.
+	for row in reader[3:]:
+		if EVAR == 0: # 横軸が整数値
+			x.append(int(row[EVAR]))
+		else:         # 横軸が時刻表示
+			x.append(row[EVAR])
 
-  reader = list(csv.reader(f))  # csv.readerはインデックス作成をサポートしていない.
+		if TVAR == 0: # 縦軸が加速度[g]
+			y1.append(float(row[2]))
+			y2.append(float(row[3]))
+			y3.append(float(row[4]))
+		else:         # 縦軸が角速度[dps]
+			y1.append(float(row[5]))
+			y2.append(float(row[6]))
+			y3.append(float(row[7]))
 
-  for row in reader[3:]:
-    if EVAR == 0: # 横軸が整数値
-      x.append(int(row[EVAR]))
-    else:         # 横軸が時刻表示
-      x.append(row[EVAR])
-
-    if TVAR == 0: # 縦軸が加速度[g]
-      y1.append(float(row[2]))
-      y2.append(float(row[3]))
-      y3.append(float(row[4]))
-    else:         # 縦軸が角速度[dps]
-      y1.append(float(row[5]))
-      y2.append(float(row[6]))
-      y3.append(float(row[7]))
-    
 if EVAR == 0:
-  #strx = "整数値"
-  strx = "Integer"
+	#strx = "整数値"
+	strx = "Integer"
 else:
-  #strx = "時刻"
-  strx = "Time"
+	#strx = "時刻"
+	strx = "Time"
 
 if TVAR == 0:
-  #stry = "加速度x/y/z[g]"
-  stry = "Acceleration x/y/z[g]"
+	#stry = "加速度x/y/z[g]"
+	stry = "Acceleration x/y/z[g]"
 else:
-  #stry = "角速度x/y/z[dps]"
-  stry = "AngularRate x/y/z[dps]"
- 
+	#stry = "角速度x/y/z[dps]"
+	stry = "AngularRate x/y/z[dps]"
+
 plt.title(str)
 plt.xlabel(strx)
 plt.ylabel(stry)
