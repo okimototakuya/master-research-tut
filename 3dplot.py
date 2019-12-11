95b00d4e3caea5c72612e76532d1d91c116ca796
沖本卓也 <okimototakuya@okimototakuyanoMacBook-Air.local>
2019-12-11 20:30:59 +0900

    3dplot.pyのインデント整形

--- 3dplot.py
+++ 3dplot.py
@@ -5,29 +5,29 @@ import acceleration_plot as ap
 import location_plot as lp
 
 def main():
-  dm_acc = ap.dataframe_maker()
-  dm_acc.init()
-  dm_loc = lp.dataframe_maker()
-  dm_loc.init()
-  dm_loc.makeLatLon()
+	dm_acc = ap.dataframe_maker()
+	dm_acc.init()
+	dm_loc = lp.dataframe_maker()
+	dm_loc.init()
+	dm_loc.makeLatLon()
 
-  #print((dm_loc.df).loc[:,['lat','lon']])
+	#print((dm_loc.df).loc[:,['lat','lon']])
 
-  fig = plt.figure()
-  #ax = fig.gca(projection='3d')
-  ax = Axes3D(fig)
-  #ax.plot((dm_loc.df).loc[:,['lat','lon']],3)
+	fig = plt.figure()
+	#ax = fig.gca(projection='3d')
+	ax = Axes3D(fig)
+	#ax.plot((dm_loc.df).loc[:,['lat','lon']],3)
 
-  x = (dm_loc.df).loc[:,['lat']]
-  y = (dm_loc.df).loc[:,['lon']]
-  z = (dm_acc.df).iloc[:len(dm_loc.df),1]  # 1:Acceleration_x
-  ax.plot(x, y, z, color='green')
-  #print(len(dm_loc.df))
-  #print(len((dm_acc.df).iloc[:len(dm_loc.df),1]))
+	x = (dm_loc.df).loc[:,['lat']]
+	y = (dm_loc.df).loc[:,['lon']]
+	z = (dm_acc.df).iloc[:len(dm_loc.df),1]  # 1:Acceleration_x
+	ax.plot(x, y, z, color='green')
+	#print(len(dm_loc.df))
+	#print(len((dm_acc.df).iloc[:len(dm_loc.df),1]))
 
-  #print((dm_acc.df).iloc[:5,1])
-  #print(len(dm_loc.df))
-  plt.show()
+	#print((dm_acc.df).iloc[:5,1])
+	#print(len(dm_loc.df))
+	plt.show()
 
 if __name__ == '__main__':
-  main()
+	main()
