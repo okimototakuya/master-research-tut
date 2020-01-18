660eae50c8bf010b1baa47179692548728a49404
沖本卓也 <okimototakuya@okimototakuyanoMacBook-Air.local>
2020-01-18 19:02:56 +0900

    特徴量数を2（加速度,角速度x）に変更したら,プログラム通る

--- hmm.py
+++ hmm.py
@@ -1,11 +1,14 @@
 import numpy as np
 import acceleration_plot as ap
 from hmmlearn import hmm
+import pandas as pd
 
 acc = ap.dataframe_maker()
 acc.init()
-#print((acc.df).iloc[:,1])		# 1:Acceleration_x
 
 model = hmm.GaussianHMM(n_components=3, covariance_type="full")
-X = (acc.df).iloc[:,1]
+X1 = (acc.df).iloc[:,1]
+X1 = pd.DataFrame(X1)
+X2 = (acc.df).iloc[:,4]
+X = X1.join(X2)
 model.fit(X)
