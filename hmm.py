9a4b7165f0a42ca35ce362a956a6e60489ae3457
沖本卓也 <okimototakuya@okimototakuyanoMacBook-Air.local>
2020-01-18 19:12:26 +0900

    とりあえず学習結果を表示させてみた.

--- hmm.py
+++ hmm.py
@@ -12,3 +12,9 @@ X1 = pd.DataFrame(X1)
 X2 = (acc.df).iloc[:,4]
 X = X1.join(X2)
 model.fit(X)
+
+print("初期確率:", model.startprob_)
+print("平均値:", model.means_)
+print("共分散値:", model.covars_)
+print("遷移確率:", model.transmat_)
+print("精度:", model.score(X))
