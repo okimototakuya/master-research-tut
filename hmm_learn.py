19f267b41e6b6c4ceee21901600df556639da21f
沖本卓也 <okimototakuya@okimototakuyanoMacBook-Air.local>
2020-01-30 14:44:08 +0900

    グローバル変数をpredのみにした. → acceleration_plot.pyから, predの取得に成功.

--- hmm_learn.py
+++ hmm_learn.py
@@ -9,23 +9,7 @@ def getPred():
 	return pred
 
 def hmmLearn():
-	global model
-	global X
 	global pred
-	model.fit(X)
-	np.set_printoptions(threshold=np.inf)		# 配列の要素を全て表示(状態系列)
-	#print("初期確率\n", model.startprob_)
-	#print("平均値\n", model.means_)
-	#print("共分散値\n", model.covars_)
-	#print("遷移確率\n", model.transmat_)
-	#print("対数尤度\n", model.score(X))
-	pred = model.predict(X)
-	print("状態系列の復号\n", pred)
-
-def main():
-	hmmLearn()
-
-if __name__ == '__main__':
 	# 加速度データのDataFrame型変数を作成.
 	acc = ap.dataframe_maker()
 	acc.init()
@@ -36,6 +20,22 @@ if __name__ == '__main__':
 	X1 = pd.DataFrame(X1)
 	X2 = (acc.df).iloc[:,4]
 	X = X1.join(X2)
+	model.fit(X)
+
+	np.set_printoptions(threshold=np.inf)		# 配列の要素を全て表示(状態系列)
+	#print("初期確率\n", model.startprob_)
+	#print("平均値\n", model.means_)
+	#print("共分散値\n", model.covars_)
+	#print("遷移確率\n", model.transmat_)
+	#print("対数尤度\n", model.score(X))
+	pred = model.predict(X)
+	#print("状態系列の復号\n", pred)
+
+def main():
+	#hmmLearn()
+	getPred()
+
+if __name__ == '__main__':
 	# 予測値を格納する変数
 	pred = None
 	# main関数の実行
