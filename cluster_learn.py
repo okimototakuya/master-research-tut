48637538a7af6952e0a3b3e77e879e220befb412
沖本卓也 <okimototakuya@okimototakuyanoMacBook-Air.local>
2020-02-07 13:37:52 +0900

    cluster_learn.pyの作成

---
+++ cluster_learn.py
@@ -0,0 +1,43 @@
+import numpy as np
+import acceleration_plot as ap
+from sklearn.cluster import KMeans
+import pandas as pd
+
+def getPred():
+	global pred
+	clusterLearn()
+	return pred
+
+def clusterLearn():
+	global pred
+	# 加速度データのDataFrame型変数を作成.
+	acc = ap.dataframe_maker()
+	acc.init()
+	# 確率モデル(Kmeansアルゴリズムの作成.
+	model = KMeans(n_clusters = 3)
+	# DataFrame型変数から学習に用いる加速度データを抽出.
+	X1 = (acc.df).iloc[:,1]
+	X1 = pd.DataFrame(X1)
+	X2 = (acc.df).iloc[:,4]
+	X = X1.join(X2)
+	X = np.array(X)		# KMeansの引数はpd.DataFrameではなくnp.array
+	model.fit(X)
+
+	#np.set_printoptions(threshold=np.inf)		# 配列の要素を全て表示(状態系列)
+	#print("初期確率\n", model.startprob_)
+	#print("平均値\n", model.means_)
+	#print("共分散値\n", model.covars_)
+	#print("遷移確率\n", model.transmat_)
+	#print("対数尤度\n", model.score(X))
+	pred = model.labels_
+	print("状態系列の復号\n", pred)
+
+def main():
+	#clusterLearn()
+	getPred()
+
+if __name__ == '__main__':
+	# 予測値を格納する変数
+	pred = None
+	# main関数の実行
+	main()
