1d05d11db1413c01d679938ca7abe293aae63071
沖本卓也 <okimototakuya@okimototakuyanoMacBook-Air.local>
2020-09-28 15:09:14 +0900

    関数aveDataについて、例外処理を追加.

--- main/config.py
+++ main/config.py
@@ -1,18 +1,22 @@
 'プロジェクトのグローバル変数を定義するクラス'
+import sys
 from pprint import pprint
 from collections import OrderedDict
 import pandas as pd
 
 
+## HACK
+## 方法1.python公式ドキュメント(https://docs.python.org/ja/3/faq/programming.html):グローバル変数モジュールのグローバル変数はカプセル化せず、剥き出し.
+## 方法2.実践Python3:シングルトンデザインパターンでは、変数をプライベート化し、変数を取得するメソッドをパブリック化.
 ' 確率モデルによる予測値'
-pred=None
+pred = None
 
 ' 加速度データファイル(csv)のパス'
-filename="../dataset/LOG_20181219141837_00010533_0021002B401733434E45.csv"  # ID16
-#filename="../dataset/LOG_20181219141901_00007140_00140064401733434E45.csv"  # ID19
+filename = "../dataset/LOG_20181219141837_00010533_0021002B401733434E45.csv"  # ID16
+#filename = "../dataset/LOG_20181219141901_00007140_00140064401733434E45.csv"  # ID19
 
 ' 加速度の方向名のリスト'
-acc=[
+acc = [
     'Acceleration_x',
     'Acceleration_y',
     'Acceleration_z',
@@ -22,24 +26,23 @@ acc=[
     ]
 
 ' 時系列/加速度2次元プロット画像ファイルの保存先'
-#path="/Users/okimototakuya/Desktop/研究データ/サンプル2件/ID16/hmm1x1y1z70000-80000_100"
-path="/Users/okimototakuya/Desktop/研究データ/サンプル2件/ID16/hoge-hoge"
-
-#path="/Users/okimototakuya/Library/Mobile Documents/com~apple~CloudDocs/Documents/研究/M1/研究データ/サンプル2件/ID16/hmm1x1y1z70000-80000_100"
-#path="/Users/okimototakuya/Desktop/tmp"
+#path = "/Users/okimototakuya/Desktop/研究データ/サンプル2件/ID16/hmm1x1y1z70000-80000_100"
+path = "/Users/okimototakuya/Desktop/研究データ/サンプル2件/ID16/hoge-hoge"
+#path = "/Users/okimototakuya/Library/Mobile Documents/com~apple~CloudDocs/Documents/研究/M1/研究データ/サンプル2件/ID16/hmm1x1y1z70000-80000_100"
+#path = "/Users/okimototakuya/Desktop/tmp"
 
 ' 1つのグラフにおけるプロット数'
-plotseg=10000
-#plotseg=131663
+plotseg = 10000
+#plotseg = 131663
 
 ' 加速度データファイルで、隠れマルコフモデルを適用させる範囲:始まり'
-hmmstart=60000
+hmmstart = 60000
 
 ' ":終わり'
-hmmend=69999
+hmmend = 69999
 
 ' 加工した加速度データファイルを格納するDataFrame型変数'
-dataframe=None
+dataframe = None
 
 '平均値をとる要素数'
 AVERAGE = 10
@@ -49,10 +52,16 @@ AVERAGE = 10
 def aveData(input_dataframe):
     '加速度の平均値をとり、DataFrame型変数にして返す'
     # 加速度の平均値を格納するためのDataFrame型変数
-    #ave_dataframe = pd.DataFrame(index=[], columns=acc)
-    ave_dataframe = pd.DataFrame(index=[], columns=list(input_dataframe.columns))
-    for i in range(int(len(input_dataframe)/AVERAGE)):
-        ave_dataframe = ave_dataframe.append(input_dataframe.iloc[i*AVERAGE:i*AVERAGE+AVERAGE, :].mean(), ignore_index=True)
+    try:
+        ave_dataframe = pd.DataFrame(index=[], columns=list(input_dataframe.columns))
+        for i in range(int(len(input_dataframe)/AVERAGE)):
+            ave_dataframe = ave_dataframe.append(input_dataframe.iloc[i*AVERAGE:i*AVERAGE+AVERAGE, :].mean(), ignore_index=True)
+    except ZeroDivisionError as err:
+        print('平均値をとる要素数(AVERAGE)が０です.')
+        sys.exit()
+    except Exception as other:
+        print('原因不明のエラーです.')
+        sys.exit()
     return ave_dataframe
 
 
@@ -69,5 +78,5 @@ def main():
     #pprint(globals())
     #pprint(OrderedDict(globals()))
 
-if __name__ =='__main__':
+if __name__ == '__main__':
     main()
--- tests/test_config.py
+++ tests/test_config.py
@@ -123,6 +123,7 @@ class TestConfig(unittest.TestCase):
     def test_random_aveData(self):
         'configモジュールの関数(aveData)をテストする関数'
         '引数のデータフレーム型オブジェクトの要素はnp.random.randで作成'
+        'FIXME:config.AVERAGE=input_rowsでなければ、テストが通らない.→少なくともテストコードに問題あり.'
         input_rows = 10
         input_columns = 3
         df_rand = make_random_dataframe(row=input_rows, columns=input_columns)
