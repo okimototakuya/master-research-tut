ccbfd0673b010d8ed4abdbfe0dd1a4004754ee31
沖本卓也 <okimototakuya@okimototakuyanoMacBook-Air.local>
2020-10-20 18:10:57 +0900

    acceleration_plot2.pyについて、Globalクラスを削除.

--- main/acceleration_plot2.py
+++ main/acceleration_plot2.py
@@ -15,106 +15,6 @@ import cluster_learn
 
 
 ##################################################################
-##########グローバル変数クラス####################################
-##################################################################
-#class Global():
-#    'acceleration_plot2モジュールのグローバル変数を属性に持つクラス'
-#    #' 確率モデルによる予測値'
-#    #pred_by_prob_model=None,
-#    ' 加速度データファイル(csv)のパス'
-#    data_read_by_api="../dataset/LOG_20181219141837_00010533_0021002B401733434E45.csv",  # ID16
-#    #data_read_by_api="../dataset/LOG_20181219141901_00007140_00140064401733434E45.csv",  # ID19
-#    ' 加速度の方向名のリスト'
-#    direct_acc=[
-#        'Acceleration_x',
-#        'Acceleration_y',
-#        'Acceleration_z',
-#        #'AngularRate_x',
-#        #'AngularRate_y',
-#        #'AngularRate_z',
-#        ],
-#    ' 時系列/加速度2次元プロット画像ファイルの保存先'
-#    #save_graph_to_path="/Users/okimototakuya/Desktop/研究データ/サンプル2件/ID16/hmm1x1y1z70000-80000_100",
-#    save_graph_to_path="/Users/okimototakuya/Desktop/研究データ/サンプル2件/ID16/hoge-hoge",
-#    #save_graph_to_path="/Users/okimototakuya/Library/Mobile Documents/com~apple~CloudDocs/Documents/研究/M1/研究データ/サンプル2件/ID16/hmm1x1y1z70000-80000_100",
-#    #save_graph_to_path="/Users/okimototakuya/Desktop/tmp",
-#    ' 1つのグラフにおけるプロット数'
-#    plot_amount_in_graph=10000,
-#    #plot_amount_in_graph=131663,
-#    ' 加速度データファイルで、隠れマルコフモデルを適用させる範囲:始まり'
-#    data_sampled_first=60000,
-#    ' ":終わり'
-#    data_sampled_last=69999,
-#    ' 加工した加速度データファイルを格納するDataFrame型変数'
-#    #data_sampled_by_func=None,
-#
-#    'Globalオブジェクト作成時に使用'
-#    #def __init__(self, **kwargs):
-#    #    self.hidden_pred_by_prob_model = kwargs["input_pred_by_prob_model"]
-#    #    self.hidden_data_read_by_api = kwargs["input_data_read_by_api"]
-#    #    self.hidden_direct_acc = kwargs["input_direct_acc"]
-#    #    self.hidden_save_graph_to_path = kwargs["input_save_graph_to_path"]
-#    #    self.hidden_plot_amount_in_graph = kwargs["input_plot_amount_in_graph"]
-#    #    self.hidden_data_sampled_first = kwargs["input_data_sampled_first"]
-#    #    self.hidden_data_sampled_last = kwargs["input_data_sampled_last"]
-#    #    self.hidden_data_sampled_by_func = kwargs["input_data_sampled_by_func"]
-#
-#    #'ゲッターメソッド'
-#    #def get_pred_by_prob_model(self):
-#    #    return self.hidden_pred_by_prob_model
-#    #def get_data_read_by_api(self):
-#    #    return self.hidden_data_read_by_api
-#    #def get_direct_acc(self):
-#    #    return self.hidden_direct_acc
-#    #def get_save_graph_to_path(self):
-#    #    return self.hidden_save_graph_to_path
-#    #def get_plot_amount_in_graph(self):
-#    #    return self.hidden_plot_amount_in_graph
-#    #def get_data_sampled_first(self):
-#    #    return self.hidden_data_sampled_first
-#    #def get_data_sampled_last(self):
-#    #    return self.hidden_data_sampled_last
-#    #def get_data_sampled_by_func(self):
-#    #    return self.hidden_data_sampled_by_func
-#
-#    #'セッターメソッド'
-#    ## 確率モデルによる予測値
-#    #def set_pred_by_prob_model(self, input_pred_by_prob_model):
-#    #    self.hidden_pred_by_prob_model = input_pred_by_prob_model
-#    ## 加速度データファイル(csv)のパス
-#    #def set_data_read_by_api(self, input_data_read_by_api):
-#    #    self.hidden_data_read_by_api = input_data_read_by_api
-#    ## 加速度の方向名のリスト
-#    #def set_direct_acc(self, input_direct_acc):
-#    #    self.hidden_direct_acc = input_direct_acc
-#    ## 時系列/加速度2次元プロット画像ファイルの保存先
-#    #def set_save_graph_to_path(self, input_save_graph_to_path):
-#    #    self.hidden_save_graph_to_path = input_save_graph_to_path
-#    ## 1つのグラフにおけるプロット数
-#    #def set_plot_amount_in_graph(self, input_plot_amount_in_graph):
-#    #    self.hidden_plot_amount_in_graph = input_plot_amount_in_graph
-#    ## 加速度データファイルで、隠れマルコフモデルを適用させる範囲:始まり
-#    #def set_data_sampled_first(self, input_data_sampled_first):
-#    #    self.hidden_data_sampled_first = input_data_sampled_first
-#    ## ":終わり
-#    #def set_data_sampled_last(self, input_data_sampled_last):
-#    #    self.hidden_data_sampled_last = input_data_sampled_last
-#    ## 加工した加速度データファイルを格納するDataFrame型変数
-#    #def set_data_sampled_by_func(self, input_data_sampled_by_func):
-#    #    self.hidden_data_sampled_by_func = input_data_sampled_by_func
-#
-#    #'プロパティ'
-#    #pred_by_prob_model = property(get_pred_by_prob_model, set_pred_by_prob_model)
-#    #data_read_by_api = property(get_data_read_by_api, set_data_read_by_api)
-#    #direct_acc = property(get_direct_acc, set_direct_acc)
-#    #save_graph_to_path = property(get_save_graph_to_path, set_save_graph_to_path)
-#    #plot_amount_in_graph = property(get_plot_amount_in_graph, set_plot_amount_in_graph)
-#    #data_sampled_first = property(get_data_sampled_first, set_data_sampled_first)
-#    #data_sampled_last = property(get_data_sampled_last, set_data_sampled_last)
-#    #data_sampled_by_func = property(get_data_sampled_by_func, set_data_sampled_by_func)
-
-
-##################################################################
 ##########例外クラス##############################################
 ##################################################################
 class WrongArgumentException(Exception):
@@ -329,30 +229,6 @@ class Acc1Acc2ColorDataframePlotter(DataframePlotter):
 def main():
     '確率モデルを適用し、学習結果を時系列表示する'
     'もしくは、加速度データを2次元プロットする'
-
-    'グローバル変数のセット'
-    #global_parameter = Global(
-    #    input_pred_by_prob_model=None,
-    #    input_data_read_by_api="../dataset/LOG_20181219141837_00010533_0021002B401733434E45.csv",  # ID16
-    #    #input_data_read_by_api="../dataset/LOG_20181219141901_00007140_00140064401733434E45.csv",  # ID19
-    #    input_direct_acc=[
-    #        'Acceleration_x',
-    #        'Acceleration_y',
-    #        'Acceleration_z',
-    #        #'AngularRate_x',
-    #        #'AngularRate_y',
-    #        #'AngularRate_z',
-    #        ],
-    #    #input_save_graph_to_path="/Users/okimototakuya/Desktop/研究データ/サンプル2件/ID16/hmm1x1y1z70000-80000_100",
-    #    input_save_graph_to_path="/Users/okimototakuya/Desktop/研究データ/サンプル2件/ID16/hoge-hoge",
-    #    #input_save_graph_to_path="/Users/okimototakuya/Library/Mobile Documents/com~apple~CloudDocs/Documents/研究/M1/研究データ/サンプル2件/ID16/hmm1x1y1z70000-80000_100",
-    #    #input_save_graph_to_path="/Users/okimototakuya/Desktop/tmp",
-    #    input_plot_amount_in_graph=10000,
-    #    #input_plot_amount_in_graph=131663,
-    #    input_data_sampled_first=60000,
-    #    input_data_sampled_last=69999,
-    #    input_data_sampled_by_func=None,
-    #    )
 
     '加速度データのDataFrame型変数を属性とする、DataframeMaker型オブジェクトを作成'
     buf = "../dataset/buf.csv"  # 加工したcsvファイルの保存先
