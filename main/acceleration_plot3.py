55711fede77ea5b41df952f59e5efbeae8cf34ce
沖本卓也 <okimototakuya@okimototakuyanoMacBook-Air.local>
2020-11-30 15:13:40 +0900

    ・tests/test_acceleration_plot3.py 　- read_csv_関数をテスト：（ほぼ）Mockオブジェクト 　-- 完全なMockオブジェクトだと、pandasがエラーを返した(pandas独自エラー：columnが割り当てられません) 　-- column(列)を現実のデータ(デンソーから配布)と一致させた 　-- row(行)数をAMOUNT_OF_ROWにした 　-- value(要素)は確率分布に従って生成される 　-- read_csv_関数について、pd.read_csvのオプションを工夫し、テストDataFrame型変数と値を一致させた

--- main/acceleration_plot3.py
+++ main/acceleration_plot3.py
@@ -2,7 +2,17 @@ import pandas as pd
 
 
 def read_csv_(input_path_to_csv):
-    return pd.read_csv(input_path_to_csv)
+    return pd.read_csv(
+            input_path_to_csv,  # 入力のcsvファイルパス
+            index_col = 0,  # 列0 (列名オブジェクトがNone) をインデックスに
+            skiprows = 1,   # 列名の行に欠損値 (None) が含まれるため、スキップしユーザが再定義
+            header = None,
+            names = ['Unnamed: 0', 'line', 'time',
+                'Acceleration(X)[g]', 'Acceleration(Y)[g]', 'Acceleration(Z)[g]',
+                'AngularRate(X)[dps]', 'AngularRate(Y)[dps]', 'AngularRate(Z)[dps]',
+                'Temperature[degree]', 'Pressure[hPa]', 'MagnetCount', 'MagnetSwitch',
+                'onCrossroad', 'crossroadID'],
+            )
 
 
 def main():
--- tests/test_acceleration_plot3.py
+++ tests/test_acceleration_plot3.py
@@ -23,39 +23,40 @@ class IterAddMicrosecond():
         'イテレータプロトコル'
         p = 0.6 # 1マイクロ秒足す確率
         for _ in range(AMOUNT_OF_ROW):
-            #self.date_time = self.date_time + datetime.timedelta(microseconds=np.random.binomial(1, p)*100000) # ベルヌーイ分布に従って1マイクロ秒加算
-            self.date_time = self.date_time + datetime.timedelta(microseconds=1*100000) # 常に1マイクロ秒加算
+            self.date_time = self.date_time + datetime.timedelta(microseconds=np.random.binomial(1, p)*100000) # ベルヌーイ分布に従って1マイクロ秒加算
+            #self.date_time = self.date_time + datetime.timedelta(microseconds=1*100000) # 常に1マイクロ秒加算
             yield self.date_time.strftime('%M:%S.%f')
 
 
-#bool_crossroad = np.random.binomial(1, 0.01, AMOUNT_OF_ROW) # onCrossroad/crossroadID特徴量を構成するNumpy配列:ベルヌーイ分布に従ってブール値を出力
-bool_crossroad = 0  # onCrossroad/crossroadID特徴量を構成するNumpy配列:常に0を出力
-str_datetime_inc_microsecond = [ms for ms in IterAddMicrosecond(datetime.datetime(2018, 12, 19, 14, minute=00, second=00, microsecond=0))]  # time特徴量を構成するリスト(各要素は文字列型)
-df_real_columns = pd.DataFrame({    # テストDataFrame型変数
-    'Unnamed: 0':range(AMOUNT_OF_ROW),
-    'line':range(AMOUNT_OF_ROW),
-    'time':str_datetime_inc_microsecond,
-    #'Acceleration(X)[g]':np.random.rand(AMOUNT_OF_ROW)*10-5,   # 一様分布に従って値を出力
-    #'Acceleration(Y)[g]':np.random.rand(AMOUNT_OF_ROW)*10-5,   # 一様分布に従って値を出力
-    #'Acceleration(Z)[g]':np.random.rand(AMOUNT_OF_ROW)*10-5,   # 一様分布に従って値を出力
-    #'AngularRate(X)[dps]':np.random.rand(AMOUNT_OF_ROW)*600-300,   # 一様分布に従って値を出力
-    #'AngularRate(Y)[dps]':np.random.rand(AMOUNT_OF_ROW)*600-300,   # 一様分布に従って値を出力
-    #'AngularRate(Z)[dps]':np.random.rand(AMOUNT_OF_ROW)*600-300,   # 一様分布に従って値を出力
-    'Acceleration(X)[g]':np.ones(AMOUNT_OF_ROW, dtype=int),
-    'Acceleration(Y)[g]':np.ones(AMOUNT_OF_ROW, dtype=int),
-    'Acceleration(Z)[g]':np.ones(AMOUNT_OF_ROW, dtype=int),
-    'AngularRate(X)[dps]':np.ones(AMOUNT_OF_ROW, dtype=int),
-    'AngularRate(Y)[dps]':np.ones(AMOUNT_OF_ROW, dtype=int),
-    'AngularRate(Z)[dps]':np.ones(AMOUNT_OF_ROW, dtype=int),
-    #'Temperature[degree]':np.random.randn(AMOUNT_OF_ROW)+18,   # 正規分布に従って値を出力
-    #'Pressure[hPa]':np.random.randn(AMOUNT_OF_ROW)+1017,   # 正規分布に従って値を出力
-    'Temperature[degree]':np.ones(AMOUNT_OF_ROW, dtype=int),
-    'Pressure[hPa]':np.ones(AMOUNT_OF_ROW, dtype=int),
-    'MagnetCount':np.zeros(AMOUNT_OF_ROW, dtype=int),
-    'MagnetSwitch':np.zeros(AMOUNT_OF_ROW, dtype=int),
-    'onCrossroad':bool_crossroad,
-    'crossroadID':bool_crossroad,
-    })
+df_real_columns = pd.DataFrame(
+    {    # テストDataFrame型変数
+        'Unnamed: 0':range(AMOUNT_OF_ROW),
+        'line':range(AMOUNT_OF_ROW),
+        'time':[ms for ms in IterAddMicrosecond(datetime.datetime(2018, 12, 19, 14, minute=00, second=00, microsecond=0))],
+        'Acceleration(X)[g]':np.random.rand(AMOUNT_OF_ROW)*10-5,   # 一様分布に従って値を出力
+        'Acceleration(Y)[g]':np.random.rand(AMOUNT_OF_ROW)*10-5,   # 一様分布に従って値を出力
+        'Acceleration(Z)[g]':np.random.rand(AMOUNT_OF_ROW)*10-5,   # 一様分布に従って値を出力
+        'AngularRate(X)[dps]':np.random.rand(AMOUNT_OF_ROW)*600-300,   # 一様分布に従って値を出力
+        'AngularRate(Y)[dps]':np.random.rand(AMOUNT_OF_ROW)*600-300,   # 一様分布に従って値を出力
+        'AngularRate(Z)[dps]':np.random.rand(AMOUNT_OF_ROW)*600-300,   # 一様分布に従って値を出力
+        #'Acceleration(X)[g]':np.ones(AMOUNT_OF_ROW, dtype=int), # 常に値1を出力
+        #'Acceleration(Y)[g]':np.ones(AMOUNT_OF_ROW, dtype=int), # 常に値1を出力
+        #'Acceleration(Z)[g]':np.ones(AMOUNT_OF_ROW, dtype=int), # 常に値1を出力
+        #'AngularRate(X)[dps]':np.ones(AMOUNT_OF_ROW, dtype=int), # 常に値1を出力
+        #'AngularRate(Y)[dps]':np.ones(AMOUNT_OF_ROW, dtype=int), # 常に値1を出力
+        #'AngularRate(Z)[dps]':np.ones(AMOUNT_OF_ROW, dtype=int), # 常に値1を出力
+        'Temperature[degree]':np.random.randn(AMOUNT_OF_ROW)+18,   # 正規分布に従って値を出力
+        'Pressure[hPa]':np.random.randn(AMOUNT_OF_ROW)+1017,   # 正規分布に従って値を出力
+        #'Temperature[degree]':np.ones(AMOUNT_OF_ROW, dtype=int), # 常に値1を出力
+        #'Pressure[hPa]':np.ones(AMOUNT_OF_ROW, dtype=int), # 常に値1を出力
+        'MagnetCount':np.zeros(AMOUNT_OF_ROW, dtype=int), # 常に値0を出力
+        'MagnetSwitch':np.zeros(AMOUNT_OF_ROW, dtype=int), # 常に値0を出力
+        #'onCrossroad':0, # 常に値0を出力
+        #'crossroadID':0, # 常に値0を出力
+        'onCrossroad':np.random.binomial(1, 0.01, AMOUNT_OF_ROW), #ベルヌーイ分布に従ってブール値を出力
+        'crossroadID':np.random.binomial(1, 0.01, AMOUNT_OF_ROW), #ベルヌーイ分布に従ってブール値を出力
+    },
+    )
 
 
 class TestAccelerationPlot3(unittest.TestCase):
@@ -63,12 +64,12 @@ class TestAccelerationPlot3(unittest.TestCase):
     'FIXME1:2020.11.27:test_save_dataframe_to_csv_とtest_read_csv_real_columnsを同時に走らせるとテストが通らない→一方のみだと通る'
     'FIXME1:2020.11.27:究極的にはtest_read_csv_real_columnsが通れば良いから、大した問題ではない'
 
-    def _test_save_dataframe_to_csv_(self):
-        'テストDataFrame型変数をテストcsvファイルに変換できたかテスト(テストコードのみの関数)'
-        df_real_columns.to_csv('./test_dataset/demo.csv')
-        #subprocess.call(['sed', '\'1', 's/,//\'', './test_dataset/demo.csv', '>', './test_dataset/demo.csv'])
-        subprocess.getoutput('sed -i -e \'1 s/,//\' ./test_dataset/demo.csv')   # 書き出したテストcsvファイルの先頭行頭のカンマを削除
-        self.assertTrue(glob.glob('./test_dataset/demo.csv'))
+    #def _test_save_dataframe_to_csv_(self):
+    #    'テストDataFrame型変数をテストcsvファイルに変換できたかテスト(テストコードのみの関数)'
+    #    df_real_columns.to_csv('./test_dataset/demo.csv')
+    #    #subprocess.call(['sed', '\'1', 's/,//\'', './test_dataset/demo.csv', '>', './test_dataset/demo.csv'])
+    #    subprocess.getoutput('sed -i -e \'1 s/,//\' ./test_dataset/demo.csv')   # 書き出したテストcsvファイルの先頭行頭のカンマを削除
+    #    self.assertTrue(glob.glob('./test_dataset/demo.csv'))
 
     #def _test_read_csv_one_column(self):
     #    'テストcsvファイルをDataFrame型変数として読み込めたかテスト(特徴量数1)'
@@ -78,10 +79,17 @@ class TestAccelerationPlot3(unittest.TestCase):
 
     def test_read_csv_real_columns(self):
         'テストcsvファイルをDataFrame型変数として読み込めたかテスト'
+        '1. テストcsvファイルを書込'
+        df_real_columns.to_csv('./test_dataset/demo.csv')
+        '2. テストcsvファイルを読込'
         df_test = ap.read_csv_('./test_dataset/demo.csv')
+        subprocess.getoutput('head ./test_dataset/demo.csv')
+        print(df_real_columns)
+        print()
+        print(df_test)
         pd.testing.assert_frame_equal(df_test, df_real_columns)
+        os.remove('./test_dataset/demo.csv')   # 次回のテストのためにテストcsvファイルを消去
 
 
 if __name__ == '__main__':
     unittest.main()
-    #os.remove('./test_dataset/demo.csv')   # 次回のテストのためにテストcsvファイルを消去
