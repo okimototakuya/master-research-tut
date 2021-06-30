1f1fcaa619614a70f4987c330c3e8170bdd70b79
沖本卓也 <okimototakuya@okimototakuyanoMacBook-Air.local>
2021-07-01 07:11:02 +0900

    テストスクリプトtests/test_learn_api.pyについて、書き方を微修正 その３ - setUpメソッドに、tests/test_iterモジュール内イテレータプロトコルの簡易出力テストを実装。 - setUpメソッドの仕様をよく把握していないため、あまり良い書き方でない。   実際setUpメソッドによって、意図していない反復出力となっている。

--- tests/test_learn_api.py
+++ tests/test_learn_api.py
@@ -1,21 +1,38 @@
 import os
 import glob
+import numpy as np
+import datetime
 import unittest
+
+
+class IterAdd():
+
+    def __init__(self, input_date_time):
+        self.date_time = input_date_time
+
+    def __iter__(self):
+        for _ in range(5):
+            self.date_time = self.date_time + datetime.timedelta(microseconds=np.random.binomial(1, 0.6)*100000)
+            yield self.date_time.strftime('%M:%S.%f')
 
 
 class TestApi(unittest.TestCase):
 
     def setUp(self):
-        pass
+        'イテレータプロトコルの簡易出力テスト'
+        date_time = datetime.datetime(2018, 12, 19, 14, minute=00, second=00, microsecond=0)
+        iter_add = IterAdd(date_time)
+        for i in iter_add:
+            print(i)
 
     def tearDown(self):
         pass
 
-    def test_os_path_isfile(self):
+    def _test_os_path_isfile(self):
         'os.path.isfileの学習用テスト'
         self.assertTrue(os.path.isfile('test_plot/*'))
 
-    def test_glob_glob(self):
+    def _test_glob_glob(self):
         'glob.globの学習用テスト'
         # 書き方その１: unittestでなく、標準のassert文を利用
         #assert glob.glob('./test_plot/*.png') is not None  # 的を得ていない書き方。Noneはシングルトンオブジェクト。
