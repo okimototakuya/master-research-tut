4b099b0e65cd4493ca5e918ded0cf7eba5d10214
沖本卓也 <okimototakuya@okimototakuyanoMacBook-Air.local>
2021-07-01 12:24:44 +0900

    tests/test_config.pyについて、これまでに削除した関数にかかっていたデコレータを削除 - document_it: 関数の詳細パラメータを出力する。 - print_dataframe: 関数内のローカル変数を出力するつもりだったが、実装が不十分なままだった。(関数内のローカル変数を参照する特殊メソッドが分からない)

--- tests/test_config.py
+++ tests/test_config.py
@@ -9,34 +9,6 @@ from main import config
 
 
 ##################################################################
-##########デコレータ##############################################
-##################################################################
-def document_it(func):
-    '関数クラスを引数にとり、その関数の詳細パラメータを関数クラスにして返す'
-    def new_function(*args, **kwargs):
-        print('Running function:', func.__name__)
-        print('Positional arguments:', args)
-        print('Keyword arguments:', kwargs)
-        result = func(*args, **kwargs)
-        print('Result:\n', result)
-        print('\n')
-        return result
-
-    return new_function
-
-
-## FIXME:関数内のローカル変数を参照する特殊メソッドが分からない.
-def print_dataframe(func):
-    '関数内のローカル変数を表示する'
-    def new_function(*args, **kwargs):
-        print('関数内ローカル変数:', func().__bool__())
-        result = func(*args, **kwargs)
-        return result
-
-    return new_function
-
-
-##################################################################
 ##########テストクラス############################################
 ##################################################################
 class TestConfig(unittest.TestCase):
@@ -50,14 +22,5 @@ class TestConfig(unittest.TestCase):
 
 if __name__ == '__main__':
     '関数make_allone_dataframeの詳細情報を出力'
-    #デコレータ使用時
-    #make_allone_dataframe()
-
-    #デコレータを使用せず、document_itオブジェクトを明示的に使用時
-    #cooler_make_allone_dataframe = document_it(make_allone_dataframe)
-    #cooler_make_allone_dataframe(row=10, columns=3)
-    #cooler_make_allone_dataframe2 = document_it(make_allone_dataframe)
-    #cooler_make_allone_dataframe2(row=1, columns=3)
-
     #テストの実行
     unittest. main()
