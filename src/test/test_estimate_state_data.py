3e6a4169c5ad11f65eebb6101d71a558203b8c48
okimototakuya <okimoto.takuya.sm@tut.jp>
2021-12-06 02:00:13 +0900

    test/test_estimate_state_data.pyにより、関数estimate_state_dataに与えるdf_averagedと、HMM適用後のdict_param_original['状態系列の復号']とで、シーケンスの大きさが変わらないことをテストした。 [ついでに] - 次元削減用のテストコード (PCA後に、HMMを適用する) を削除した。 - ↑テスト関数2つ

--- src/test/test_estimate_state_data.py
+++ src/test/test_estimate_state_data.py
@@ -28,7 +28,6 @@ class TestEstimateStateData(unittest.TestCase):
                             input_mean_range = ap3.MEAN_RANGE, # 引数2:平均値を計算する際の、要素数
                             input_how = ap3.HOW_TO_CALCULATE_MEAN,   # 引数3:平均値の算出方法 fixed_mean:固定(?)平均, slide_mean:移動平均, slide_median:移動中央値
                     )
-        self.df_pca = ap3.decompose_data(self.df_averaged.drop('time', axis=1)).join(self.df_averaged['time']) # 3. 主成分分析
 
     def tearDown(self):
         '''
@@ -36,47 +35,17 @@ class TestEstimateStateData(unittest.TestCase):
         '''
         pass
 
-    def test_estimate_state_data_fitting_rate_predict_signal_between_original_and_pca(self):
+    def test_estimate_state_data_return_state_sequence_which_size_is_input_df_averaged_length(self):
         '''
-        現データとPCA後データとの、状態ラベルの一致率を算出
+        関数estimate_state_dataに与えるdf_averagedと、HMM適用後のdict_param_original['状態系列の復号']とで、
+        シーケンスの大きさが同じであることを確認
         '''
-        for i in range(10):
-            dict_param_original = ap3.estimate_state_data(   # 主成分分析をせずに、隠れマルコフモデルを適用する場合
-                    input_df_averaged = self.df_averaged.drop('time', axis=1),
-                    input_how = ap3.ASSUMED_PROBABILISTIC_MODEL,
-                    input_number_of_assumed_state = ap3.NUMBER_OF_ASSUMED_STATE,
-                )
-            dict_param_pca = ap3.estimate_state_data(   # 主成分分析をして、隠れマルコフモデルを適用する場合
-                    input_df_averaged = self.df_pca.drop('time', axis=1),
-                    input_how = ap3.ASSUMED_PROBABILISTIC_MODEL,
-                    input_number_of_assumed_state = ap3.NUMBER_OF_ASSUMED_STATE,
-                )
-            num_fit = sum([1 if dict_param_original['状態系列の復号'][i] == dict_param_pca['状態系列の復号'][i] else 0 for i in range(len(dict_param_original['状態系列の復号']))])
-            per_fit = num_fit / len(dict_param_original['状態系列の復号']) * 100
-            print('試行{i}: 状態ラベルの一致率:{per_fit}'.format(i=i+1, per_fit=per_fit))
-
-    def test_estimate_state_data_fitting_rate_transition_matrix_between_original_and_pca(self):
-        '''
-        現データとPCA後データとの、遷移行列の一致率を算出
-        '''
-        for i in range(10):
-            dict_param_original = ap3.estimate_state_data(   # 主成分分析をせずに、隠れマルコフモデルを適用する場合
-                    input_df_averaged = self.df_averaged.drop('time', axis=1),
-                    input_how = ap3.ASSUMED_PROBABILISTIC_MODEL,
-                    input_number_of_assumed_state = ap3.NUMBER_OF_ASSUMED_STATE,
-                )
-            dict_param_pca = ap3.estimate_state_data(   # 主成分分析をして、隠れマルコフモデルを適用する場合
-                    input_df_averaged = self.df_pca.drop('time', axis=1),
-                    input_how = ap3.ASSUMED_PROBABILISTIC_MODEL,
-                    input_number_of_assumed_state = ap3.NUMBER_OF_ASSUMED_STATE,
-                )
-            #print(dict_param_original['遷移行列'])
-            #print(dict_param_pca['遷移行列'])
-            #bool_fit = 1 if dict_param_original['遷移行列'] == dict_param_pca['遷移行列'] else 0
-            #print('試行{i}: 遷移行列の一致率:{bool_fit}'.format(i=i+1, bool_fit=bool_fit))
-            #np.testing.assert_array_almost_equal(dict_param_original['遷移行列'], dict_param_pca['遷移行列'])
-            print('試行{i}:'.format(i=i+1))
-            print(dict_param_original['遷移行列'] - dict_param_pca['遷移行列'])
+        dict_param_original = ap3.estimate_state_data(
+                input_df_averaged = self.df_averaged.drop('time', axis=1),
+                input_how = ap3.ASSUMED_PROBABILISTIC_MODEL,
+                input_number_of_assumed_state = ap3.NUMBER_OF_ASSUMED_STATE,
+            )
+        self.assertEqual(len(self.df_averaged), len(dict_param_original['状態系列の復号']))
 
 
 if __name__ == '__main__':
