feea684658db269813d2277c2765b88effd0af2a
okimototakuya <okimoto.takuya.sm@tut.jp>
2021-12-05 17:01:12 +0900

    [実験] experiment.shを更新し、33_50, 34_50, 82, 82_50, 83, 83_50, 84, 84_50.csvについて平均区間1,10,20,30,40で保存されたことを確認した。

--- src/build/experiment.sh
+++ src/build/experiment.sh
@@ -8,8 +8,64 @@
 #python ../main/acceleration_plot3.py 40 ../../dataset/2crossroad.csv
 
 # 32crossroad_50
-python ../main/acceleration_plot3.py 1 ../../dataset/32crossroad_50.csv
-python ../main/acceleration_plot3.py 10 ../../dataset/32crossroad_50.csv
-python ../main/acceleration_plot3.py 20 ../../dataset/32crossroad_50.csv
-python ../main/acceleration_plot3.py 30 ../../dataset/32crossroad_50.csv
-python ../main/acceleration_plot3.py 40 ../../dataset/32crossroad_50.csv
+#python ../main/acceleration_plot3.py 1 ../../dataset/32crossroad_50.csv
+#python ../main/acceleration_plot3.py 10 ../../dataset/32crossroad_50.csv
+#python ../main/acceleration_plot3.py 20 ../../dataset/32crossroad_50.csv
+#python ../main/acceleration_plot3.py 30 ../../dataset/32crossroad_50.csv
+#python ../main/acceleration_plot3.py 40 ../../dataset/32crossroad_50.csv
+
+# 33crossroad_50
+python ../main/acceleration_plot3.py 1 ../../dataset/33crossroad_50.csv
+python ../main/acceleration_plot3.py 10 ../../dataset/33crossroad_50.csv
+python ../main/acceleration_plot3.py 20 ../../dataset/33crossroad_50.csv
+python ../main/acceleration_plot3.py 30 ../../dataset/33crossroad_50.csv
+python ../main/acceleration_plot3.py 40 ../../dataset/33crossroad_50.csv
+
+# 34crossroad_50
+python ../main/acceleration_plot3.py 1 ../../dataset/34crossroad_50.csv
+python ../main/acceleration_plot3.py 10 ../../dataset/34crossroad_50.csv
+python ../main/acceleration_plot3.py 20 ../../dataset/34crossroad_50.csv
+python ../main/acceleration_plot3.py 30 ../../dataset/34crossroad_50.csv
+python ../main/acceleration_plot3.py 40 ../../dataset/34crossroad_50.csv
+
+# 82crossroad
+python ../main/acceleration_plot3.py 1 ../../dataset/82crossroad.csv
+python ../main/acceleration_plot3.py 10 ../../dataset/82crossroad.csv
+python ../main/acceleration_plot3.py 20 ../../dataset/82crossroad.csv
+python ../main/acceleration_plot3.py 30 ../../dataset/82crossroad.csv
+python ../main/acceleration_plot3.py 40 ../../dataset/82crossroad.csv
+
+# 82crossroad_50
+python ../main/acceleration_plot3.py 1 ../../dataset/82crossroad_50.csv
+python ../main/acceleration_plot3.py 10 ../../dataset/82crossroad_50.csv
+python ../main/acceleration_plot3.py 20 ../../dataset/82crossroad_50.csv
+python ../main/acceleration_plot3.py 30 ../../dataset/82crossroad_50.csv
+python ../main/acceleration_plot3.py 40 ../../dataset/82crossroad_50.csv
+
+# 83crossroad
+python ../main/acceleration_plot3.py 1 ../../dataset/83crossroad.csv
+python ../main/acceleration_plot3.py 10 ../../dataset/83crossroad.csv
+python ../main/acceleration_plot3.py 20 ../../dataset/83crossroad.csv
+python ../main/acceleration_plot3.py 30 ../../dataset/83crossroad.csv
+python ../main/acceleration_plot3.py 40 ../../dataset/83crossroad.csv
+
+# 83crossroad_50
+python ../main/acceleration_plot3.py 1 ../../dataset/83crossroad_50.csv
+python ../main/acceleration_plot3.py 10 ../../dataset/83crossroad_50.csv
+python ../main/acceleration_plot3.py 20 ../../dataset/83crossroad_50.csv
+python ../main/acceleration_plot3.py 30 ../../dataset/83crossroad_50.csv
+python ../main/acceleration_plot3.py 40 ../../dataset/83crossroad_50.csv
+
+# 84crossroad
+python ../main/acceleration_plot3.py 1 ../../dataset/84crossroad.csv
+python ../main/acceleration_plot3.py 10 ../../dataset/84crossroad.csv
+python ../main/acceleration_plot3.py 20 ../../dataset/84crossroad.csv
+python ../main/acceleration_plot3.py 30 ../../dataset/84crossroad.csv
+python ../main/acceleration_plot3.py 40 ../../dataset/84crossroad.csv
+
+# 84crossroad_50
+python ../main/acceleration_plot3.py 1 ../../dataset/84crossroad_50.csv
+python ../main/acceleration_plot3.py 10 ../../dataset/84crossroad_50.csv
+python ../main/acceleration_plot3.py 20 ../../dataset/84crossroad_50.csv
+python ../main/acceleration_plot3.py 30 ../../dataset/84crossroad_50.csv
+python ../main/acceleration_plot3.py 40 ../../dataset/84crossroad_50.csv
